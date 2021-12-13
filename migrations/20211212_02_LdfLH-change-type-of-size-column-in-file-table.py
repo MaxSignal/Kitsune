d6c9651f5603bd0b567d830046e0ead256856662
Anonymous <>
2021-12-12 19:19:41 -0500

    Update 20211212_02_LdfLH-change-type-of-size-column-in-file-table.py

--- migrations/20211212_02_LdfLH-change-type-of-size-column-in-file-table.py
+++ migrations/20211212_02_LdfLH-change-type-of-size-column-in-file-table.py
@@ -8,7 +8,7 @@ __depends__ = {'20211212_01_K1PlV-add-size-and-image-hash-columns-to-file-table'
 
 steps = [
     step(
-        "ALTER TABLE assets ALTER COLUMN size TYPE bigint",
-        "ALTER TABLE assets ALTER COLUMN size TYPE int"
+        "ALTER TABLE files ALTER COLUMN size TYPE bigint",
+        "ALTER TABLE files ALTER COLUMN size TYPE int"
     )
 ]
