eaa68645715945f2f55142d607f8281d5cca8c73
Shinonome <shinonome@kemono.party>
2022-09-10 19:29:54 -0400

    fix(importer): do not relog sessions

--- src/internals/utils/encryption.py
+++ src/internals/utils/encryption.py
@@ -3,8 +3,8 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP, AES
 from Crypto.Random import get_random_bytes
 from base64 import b64decode, b64encode
+from os.path import join, exists
 from os import makedirs
-from os.path import join
 import uuid
 import config
 
@@ -25,31 +25,31 @@ def decrypt_session(file, rsa_key):
 
 
 def encrypt_and_log_session(import_id, service, key):
+    filename = f'{service}-{import_id}'
     try:
-        makedirs(base_dir, exist_ok=True)
-        data = {
-            'import_id': import_id,
-            'service': service,
-            'key': key
-        }
-        to_encrypt = json.dumps(data)
+        if not exists(join(base_dir, filename)):
+            makedirs(base_dir, exist_ok=True)
+            data = {
+                'import_id': import_id,
+                'service': service,
+                'key': key
+            }
+            to_encrypt = json.dumps(data)
 
-        key_der = b64decode(config.pubkey.strip())
-        key_pub = RSA.importKey(key_der)
-        cipher = PKCS1_OAEP.new(key_pub)
+            key_der = b64decode(config.pubkey.strip())
+            key_pub = RSA.importKey(key_der)
+            cipher = PKCS1_OAEP.new(key_pub)
 
-        new_aes_key = get_random_bytes(16)
-        aes_cipher = AES.new(new_aes_key, AES.MODE_EAX)
-        encrypted_bin_aes_key = cipher.encrypt(new_aes_key)
-        encrypted_aes_key = b64encode(encrypted_bin_aes_key)
+            new_aes_key = get_random_bytes(16)
+            aes_cipher = AES.new(new_aes_key, AES.MODE_EAX)
+            encrypted_bin_aes_key = cipher.encrypt(new_aes_key)
+            encrypted_aes_key = b64encode(encrypted_bin_aes_key)
 
-        nonce = aes_cipher.nonce
-        ciphertext, tag = aes_cipher.encrypt_and_digest(to_encrypt.encode())
-        to_write = b64encode(encrypted_aes_key + b'|' + b64encode(nonce) + b'|' + b64encode(ciphertext) + b'|' + b64encode(tag)).decode('utf-8')
+            nonce = aes_cipher.nonce
+            ciphertext, tag = aes_cipher.encrypt_and_digest(to_encrypt.encode())
+            to_write = b64encode(encrypted_aes_key + b'|' + b64encode(nonce) + b'|' + b64encode(ciphertext) + b'|' + b64encode(tag)).decode('utf-8')
 
-        filename = f'{service}-{import_id}'
-
-        with open(join(base_dir, filename), 'w') as f:
-            f.write(to_write)
+            with open(join(base_dir, filename), 'w') as f:
+                f.write(to_write)
     except Exception as e:
         print(f'Error encrypting session data. Continuing with import: {e}')
